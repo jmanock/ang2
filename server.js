a01c0cdbfce05848b61e7a56111e04a0dbb1bd31
Jon Manock <jonmanock@gmail.com>
2017-03-23 16:48:51 -0400

    finally something works right

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.set('port', 3000);
+app.use(express.static(path.join(__dirname, 'public')));
+
+var server = app.listen(app.get('port'), function(){
+  var port = server.address().port;
+  console.log('Magic is about to go down on 3000');
+});
